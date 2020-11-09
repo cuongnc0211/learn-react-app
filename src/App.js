9a65bc5099eb158f36b56c48c0f4f2d5643a0e41
cuongnc0211 <cuongnguyenfu@gmail.com>
2020-11-09 18:04:16 +0700

    toggle todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import TodoFeature from './features/Todo';
 
 function App() {
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 
 function TodoFeature(props) {
-  const todoList = [
+  const initTodoList = [
     {
       id: 1,
-      title: 'Eat'
+      title: 'Eat',
+      status: 'new'
     },
     {
       id: 2,
-      title: 'Sleep'
+      title: 'Sleep',
+      status: 'completed'
     },
     {
       id: 3,
-      title: 'Code'
+      title: 'Code',
+      status: 'new'
     },
   ]
+
+  const [todoList, setTodoList] = useState(initTodoList);
+
+  const handleTodoClick = (todo, idx) => {
+    console.log(todo, idx);
+    //clone current array to new array
+    const newTodoList = [...todoList]
+    //toggle state
+    newTodoList[idx] = {
+      ...newTodoList[idx],
+      status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
+    };
+    // update todo
+    setTodoList(newTodoList)
+  }
+
   return (
     <div>
       <h3>Todo List</h3>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }

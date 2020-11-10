05fb0583658473b3f80bd38b71d4b8519c0fc790
cuongnc0211 <cuongnguyenfu@gmail.com>
2020-11-10 19:07:29 +0700

    add route to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import { NavLink, Route, Switch, Redirect } from 'react-router-dom';
 import './App.scss';
+import AlbumFeature from './features/Album';
 import TodoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      HomePage
+      <p><NavLink to="/todos" activeClassName="active-menu">Todo</NavLink></p>
+      <p><NavLink to="/albums" activeClassName="active-menu">Albums</NavLink></p>
+
+      <Switch>
+        <Redirect from="/home" to="/" exact />
+
+        <Route path="/" component={TodoFeature} exact />
+        <Route path="/todos" component={TodoFeature} exact />
+        <Route path="/albums" component={AlbumFeature} exact />
+      </Switch>
     </div>
   );
 }
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 
+
 function TodoFeature(props) {
   const initTodoList = [
     {
